f26c5174e23ccab763a7c6968ab3c5c8dac4a575
David Stroud <david@greymatter-analytics.com>
2018-02-08 17:14:57 -0600

    Rename columns and delete na rows

--- BLT/Merging.R
+++ BLT/Merging.R
@@ -4,4 +4,16 @@ library(downloader) # For ease of downloading
 download("https://raw.githubusercontent.com/thoughtfulbloke/faoexample/master/appleorange.csv", destfile="appleorange.csv")
 download("https://raw.githubusercontent.com/thoughtfulbloke/faoexample/master/stability.csv", destfile="stability.csv")
 
+# Read in data, sAF ~ read in chr instead of factors
+ao <- read.csv("BLT/appleorange.csv", stringsAsFactors = FALSE, header = FALSE)
+str(ao)
+aoraw <- ao
+head(aoraw,10)
+str(aoraw)
+tail(aoraw,10)
 
+# Delete last rows because of multiple na
+aodata <- aoraw[3:700,]
+
+# Rename columns
+names(aodata) <- c("country", "countrynumber", "products", "productnumber","tonnes","year")
