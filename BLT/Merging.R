bc2b0613624f918d6dbdd4b6e39f4f46a54b15c6
David Stroud <david@greymatter-analytics.com>
2018-02-08 16:39:18 -0600

    File demonstrating merging

---
+++ BLT/Merging.R
@@ -0,0 +1,7 @@
+library(downloader) # For ease of downloading
+
+# Download files form the internet
+download("https://raw.githubusercontent.com/thoughtfulbloke/faoexample/master/appleorange.csv", destfile="appleorange.csv")
+download("https://raw.githubusercontent.com/thoughtfulbloke/faoexample/master/stability.csv", destfile="stability.csv")
+
+
