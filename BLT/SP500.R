e8a0f3495632ad6c7b5b42940a577a04adcb0f11
davestroud <david@greymatter-analytics.com>
2018-04-03 16:51:02 -0500

    Built vol function

--- BLT/SP500.R
+++ BLT/SP500.R
@@ -1,10 +1,27 @@
-# Download the data.
+# Libraries
+library(tseries)
 
+# Download the data.
+SNPdata <- get.hist.quote('^gspc', quote = 'Close')
 
 # Calculate log returns.
-
+SNPret <- log(lag(SNPdata)) - log(SNPdata)
 
 # Calculate volatility measure.
+SNPvol <- sd(SNPret) * sqrt(250) * 100
+
+# Vol function
+Vol <- function(d, logrets) {
+  var = 0
+  lam = 0
+  
+  varlist <- c()
+  for(r in logrets) {
+    lam = lam*(1-1/d) + 1
+    var = (1-1/lam)*var + (1/lam)*r^2
+    varlist <- c(varlist, var)
+  }
+}
 
 
 # Calculate volatility over entire length of series for various three different decay factors.
