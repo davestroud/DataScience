b3cdfb13f8be283bc83af98fac9a2cdd17c851d3
davestroud <david@greymatter-analytics.com>
2018-04-03 16:57:19 -0500

    Calculate volatility for three decay factors

--- BLT/SP500.R
+++ BLT/SP500.R
@@ -21,10 +21,12 @@ Vol <- function(d, logrets) {
     var = (1-1/lam)*var + (1/lam)*r^2
     varlist <- c(varlist, var)
   }
+  sqrt(varlist)
 }
 
-
 # Calculate volatility over entire length of series for various three different decay factors.
-
+volest <- Vol(10,SNPret)
+volest2 <- Vol(30,SNPret)
+volest3 <- Vol(100,SNPret)
 
 # Plot the results, overlaying the volatility curves on the data, just as was done in the S&P example.
