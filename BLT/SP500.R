3555104974ee2fa36879e0a737c577f155cde003
davestroud <david@greymatter-analytics.com>
2018-04-03 16:35:06 -0500

    Made initial documentation

---
+++ BLT/SP500.R
@@ -0,0 +1,13 @@
+# Download the data.
+
+
+# Calculate log returns.
+
+
+# Calculate volatility measure.
+
+
+# Calculate volatility over entire length of series for various three different decay factors.
+
+
+# Plot the results, overlaying the volatility curves on the data, just as was done in the S&P example.
