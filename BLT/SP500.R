56e67f7cb2675118cc888b247260e5e41eaf9faf
davestroud <david@greymatter-analytics.com>
2018-04-03 16:59:48 -0500

    Plot volest

--- BLT/SP500.R
+++ BLT/SP500.R
@@ -30,3 +30,11 @@ volest2 <- Vol(30,SNPret)
 volest3 <- Vol(100,SNPret)
 
 # Plot the results, overlaying the volatility curves on the data, just as was done in the S&P example.
+plot(volest,type = "l")
+
+
+
+
+
+
+
